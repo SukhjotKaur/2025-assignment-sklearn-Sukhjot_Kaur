313972a99ecd00fb92bae90643a0575ecccafb74
SukhjotKaur <skaurage@gmail.com>
2025-12-16 18:09:18 +0100

    UP my code improved

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -64,7 +64,8 @@ from sklearn.utils.multiclass import check_classification_targets
 
 class KNearestNeighbors(ClassifierMixin, BaseEstimator):
     """KNearestNeighbors classifier."""
-    def __init__(self, n_neighbors=1):  # noqa: D107
+
+    def __init__(self, n_neighbors=1):
         self.n_neighbors = n_neighbors
 
     def fit(self, X, y):
@@ -164,6 +165,7 @@ class MonthlySplit(BaseCrossValidator):
         for which this column is not a datetime, it will raise a ValueError.
         To use the index as column just set `time_col` to `'index'`.
     """
+    
     def __init__(self, time_col='index'):  # noqa: D107
         self.time_col = time_col
 
